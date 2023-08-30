6e285ad59f801b7eb3d9f3414f6ba697a6c80ccd
DIEGO HIDEKI TOKUNAGA <83131539+diegotokunaga@users.noreply.github.com>
2023-08-30 15:42:22 -0300

    Create Pessoa.java

---
+++ Bintroducaoclasses/model/Pessoa.java
@@ -0,0 +1,11 @@
+package maratona.dojo.java.CintroducaoMetodos.models;
+
+public class Pessoa {
+    public String nome;
+    public int idade;
+    public  char sexo;
+
+    public void imprime() {
+        System.out.println(this.nome);
+    }
+}
