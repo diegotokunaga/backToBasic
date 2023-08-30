63ae1027d6ebe0cf70a2aafdd1c70413383cff28
DIEGO HIDEKI TOKUNAGA <83131539+diegotokunaga@users.noreply.github.com>
2023-08-30 15:44:49 -0300

    Delete CintroducaoMetodos directory

--- CintroducaoMetodos/models/ImpressoraPessoa.java
+++
@@ -1,9 +0,0 @@
-package maratona.dojo.java.CintroducaoMetodos.models;
-
-public class ImpressoraPessoa {
-     public void imprime(Pessoa pessoa){
-          System.out.println(pessoa.nome);
-          System.out.println(pessoa.sexo);
-          System.out.println(pessoa.idade);
-     }
-}
